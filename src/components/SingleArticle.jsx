710ed606e9609f66eabcf48af15620e1d67e6049
yerayafonso <yafonso02@gmail.com>
2026-02-24 14:10:26 +0000

    install axios and add buttons to update vote

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,6 +1,7 @@
 import { Link, useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { useState } from "react";
+import axios from "axios";
 
 function SingleArticle() {
   const [singleArticle, setSingleArticle] = useState("");
@@ -28,6 +29,63 @@ function SingleArticle() {
   const body = singleArticle.body;
   const votes = singleArticle.votes;
   const commentCount = singleArticle.comment_count;
+  console.log("votes", votes);
+
+  const [voteCount, setVoteCount] = useState(0);
+
+  console.log("vote state", voteCount);
+
+  function increaseVote(article_id) {
+    setVoteCount((currentVoteCount) => {
+      return currentVoteCount + 1;
+    });
+    axios
+      .patch(
+        `https://nc-news-app-5h3i.onrender.com/api/articles/${article_id}`,
+        { inc_votes: 1 },
+      )
+      .catch(() => {
+        return setVoteCount((currentVoteCount) => {
+          return currentVoteCount - 1;
+        });
+      });
+  }
+
+  function decreaseVote(article_id) {
+    setVoteCount((currentVoteCount) => {
+      return currentVoteCount - 1;
+    });
+    axios
+      .patch(
+        `https://nc-news-app-5h3i.onrender.com/api/articles/${article_id}`,
+        { inc_votes: -1 },
+      )
+      .catch(() => {
+        return setVoteCount((currentVoteCount) => {
+          return currentVoteCount + 1;
+        });
+      });
+  }
+
+  // useEffect(() => {
+  //   async function updateVote() {
+  //     const response = await fetch(
+  //       `https://nc-news-app-5h3i.onrender.com/api/articles/${article_id}`,
+  //       {
+  //         method: "PATCH",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //         body: JSON.stringify({
+  //           changeVote: voteCount,
+  //         }),
+  //       },
+  //     );
+  //     const results = await response.json();
+  //     votes = results.votes;
+  //   }
+  //   updateVote();
+  // }, [voteCount]);
 
   return (
     <>
@@ -54,7 +112,21 @@ function SingleArticle() {
                 <p>Topic: {topic}</p>
               </li>
               <li>
-                <p>Votes: {votes}</p>
+                <p>Votes: {votes + voteCount}</p>
+                <button
+                  onClick={() => {
+                    increaseVote(article_id);
+                  }}
+                >
+                  +
+                </button>
+                <button
+                  onClick={() => {
+                    decreaseVote(article_id);
+                  }}
+                >
+                  -
+                </button>
               </li>
               <li>
                 <p>No. of Comments: {commentCount}</p>
