9731f03f870ba359126102873c7219b1acb17e4f
yerayafonso <yafonso02@gmail.com>
2026-04-07 13:52:27 +0100

    modify post comment page to navigate to comments page for a successful post

--- src/components/CommentStatus.jsx
+++ src/components/CommentStatus.jsx
@@ -1,8 +1,14 @@
+import { useNavigate } from "react-router";
+import { useParams } from "react-router-dom";
+
 function CommentStatus(props) {
   const submitComment = props.submitComment;
+  const { article_id } = useParams();
+  let navigate = useNavigate();
 
   if (submitComment === "success") {
-    return <p className="status-msg success">Comment posted</p>;
+    // return <p className="status-msg success">Comment posted</p>;
+    navigate(`/articles/${article_id}/comments`);
   } else if (submitComment === "error") {
     return (
       <p className="status-msg fail">Comment failed to post, invalid user</p>
